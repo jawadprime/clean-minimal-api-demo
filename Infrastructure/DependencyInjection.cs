69c4599ed809b3acc52cc5a39e280e4e2bc3ab4b
jawad-prime-dev <jawadahmed.dev@gmail.com>
2026-03-15 11:16:18 +0500

    Register infrastructure dependencies

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,61 @@
+﻿using Application.Repositories;
+using Infrastructure.Options;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        services.AddOptions(configuration);
+
+        services.AddDatabase(configuration);
+
+        services.AddRepositories();
+
+        return services;
+    }
+
+    private static IServiceCollection AddOptions(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+
+        services.AddOptions<DatabaseOptions>()
+            .Bind(configuration.GetSection(DatabaseOptions.SectionName))
+            .ValidateDataAnnotations();
+
+        return services;
+    }
+
+    private static IServiceCollection AddDatabase(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+
+        services.AddDbContext<AppDbContext>((sp, options) =>
+        {
+            var dbOptions = sp
+                .GetRequiredService<IOptions<DatabaseOptions>>()
+                .Value;
+
+            options.UseNpgsql(dbOptions.ApplicationDb);
+        });
+
+        return services;
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IProductRepository, ProductRepository>();
+
+        return services;
+    }
+}
