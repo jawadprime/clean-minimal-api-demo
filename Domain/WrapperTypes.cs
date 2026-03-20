75cf3175feb470afeacc8b543f4e066abcf2d05b
jawad-prime-dev <jawadahmed.dev@gmail.com>
2026-03-20 16:16:25 +0500

    Introduce strong types

---
+++ Domain/WrapperTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain;
+
+public abstract record ProductId;
+public record HasProductId(Guid Value) : ProductId;
+public record NoProductId() : ProductId;
+
+public record ProductName(string Value);
+
+public abstract record ProductCreatedAt();
+public record HasProductCreatedAt(DateTime Value) : ProductCreatedAt;
+public record NoProductCreatedAt() : ProductCreatedAt;
