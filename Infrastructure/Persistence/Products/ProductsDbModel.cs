43967188480d320fdf8b8d4abf40281d8bf14eba
jawadprime <jawadahmed.dev@gmail.com>
2026-03-24 20:50:28 +0500

    Use required on entity props

--- Infrastructure/Persistence/Products/ProductsDbModel.cs
+++ Infrastructure/Persistence/Products/ProductsDbModel.cs
@@ -5,20 +5,12 @@ using Infrastructure.Persistence.Common;
 namespace Infrastructure.Persistence.Product;
 internal class ProductsDbModel : BaseEntity<Guid>
 {
-    public string Name { get; set; }
+    public required string Name { get; set; }
     public DateTime CreatedAt { get; set; }
-    public ProductsDbModel() { }
-
-    public ProductsDbModel(Guid id, string name, DateTime createdAt)
-    {
-        Id = id;
-        Name = name;
-        CreatedAt = createdAt;
-    }
 
     public Result<Domain.Product> ToDomain() 
         => Domain.Product.Create(new(new HasProductId(Id), new(Name), new HasProductCreatedAt(CreatedAt)));
 
     public static ProductsDbModel FromDomain(Domain.Product domain)
-        => new ProductsDbModel(new Guid(), domain.Name.Value, DateTime.UtcNow);
+        => new() { Id = new Guid(), Name = domain.Name.Value, CreatedAt = DateTime.UtcNow };
 }
