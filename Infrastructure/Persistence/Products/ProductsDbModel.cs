e09d43da6a5c6c128d84533528b5bdeb2b7d80f6
jawad-prime-dev <jawadahmed.dev@gmail.com>
2026-03-24 20:43:42 +0500

    Inherit db entities from BaseEntity class

--- Infrastructure/Persistence/Products/ProductsDbModel.cs
+++ Infrastructure/Persistence/Products/ProductsDbModel.cs
@@ -1,9 +1,21 @@
 ﻿using Common.Results;
 using Domain;
+using Infrastructure.Persistence.Common;
 
 namespace Infrastructure.Persistence.Product;
-internal record ProductsDbModel (Guid Id, string Name, DateTime CreatedAt)
+internal class ProductsDbModel : BaseEntity<Guid>
 {
+    public string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public ProductsDbModel() { }
+
+    public ProductsDbModel(Guid id, string name, DateTime createdAt)
+    {
+        Id = id;
+        Name = name;
+        CreatedAt = createdAt;
+    }
+
     public Result<Domain.Product> ToDomain() 
         => Domain.Product.Create(new(new HasProductId(Id), new(Name), new HasProductCreatedAt(CreatedAt)));
 
